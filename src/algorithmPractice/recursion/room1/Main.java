088bd6cffb4045bed4b11ecb98cb060d66216467
p3tzata <p3tzata_@abv.bg>
2020-06-25 09:56:24 +0300

    2020-06-25

--- src/algorithmPractice/recursion/room1/Main.java
+++ src/algorithmPractice/recursion/room1/Main.java
@@ -3,7 +3,7 @@ package algorithmPractice.recursion.room1;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("stest");
+		System.out.println("stesfdfst");
 
 	}
 
