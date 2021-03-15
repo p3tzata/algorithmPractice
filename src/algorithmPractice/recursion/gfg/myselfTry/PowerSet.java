5aef3d8629e23b16c3ee30c2d5fe6103825d0555
p3tzata <p3tzata_@abv.bg>
2021-03-15 14:23:07 +0200

    2021-03-15

--- src/algorithmPractice/recursion/gfg/myselfTry/PowerSet.java
+++ src/algorithmPractice/recursion/gfg/myselfTry/PowerSet.java
@@ -1,17 +1,19 @@
 package algorithmPractice.recursion.gfg.myselfTry;
 
+
 import util.Util;
 
 public class PowerSet {
 
 	private static String[] array;
+	private static String result ="";
 	
 	public static void main(String[] args) {
 		
 		array = new String[] {"a","b","c"};
 		
 		
-		fun(0,"");
+		fun(0,result);
 	}
 
 	private static void fun(int b,String result) {
@@ -20,12 +22,14 @@ public class PowerSet {
 		
 		
 		for(int i=b;i<array.length;i++) {
-			Util.printlnLine(result + array[i]);
+			
+			result = result + array[i];
+			Util.printlnLine(result);
 			
 			
-			fun(i+1,result + array[i]);
+			fun(i+1,result);
 			
-			
+			result = result.substring(0,result.length()-1);
 		}
 		
 		
